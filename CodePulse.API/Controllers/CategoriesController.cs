22ee4fc89fee2a457a6aaa68f59695acc379556c
Gialap Duong <89369650+gialap210502@users.noreply.github.com>
2024-01-07 01:21:42 +0700

    category controller

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace CodePulse.API.Controllers;
+
+//https://localhost:xxxx/api/categories
+[ApiController]
+[Route("[controller]")]
+public class CategoriesController : ControllerBase
+{ 
+
+}
