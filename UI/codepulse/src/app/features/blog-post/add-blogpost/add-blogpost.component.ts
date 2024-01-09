6615c5ff7681e7a54be93557928edc91a216a17b
Gialap Duong <89369650+gialap210502@users.noreply.github.com>
2024-01-09 23:07:07 +0700

    create blog post (not conect to api yet)

---
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { AddBlogpost } from '../models/add-blog-post.model';
+
+@Component({
+  selector: 'app-add-blogpost',
+  templateUrl: './add-blogpost.component.html',
+  styleUrls: ['./add-blogpost.component.css']
+})
+export class AddBlogpostComponent {
+  model: AddBlogpost;
+
+  constructor(){
+    this.model = {
+      title: '',
+      shortDescription: '',
+      content: '',
+      featuredImageUrl: '',
+      urlHandle: '',
+      author: '',
+      publishedDate: new Date(),
+      isVisible: true
+    }
+  }
+
+  onFormSubmit(): void {
+    console.log(this.model);
+  }
+}
---
+++ UI/codepulse/src/app/features/blog-post/models/add-blog-post.model.ts
@@ -0,0 +1,10 @@
+export interface AddBlogpost {
+    title: string;
+    shortDescription: string;
+    content: string;
+    featuredImageUrl: string;
+    urlHandle: string;
+    author: string;
+    publishedDate: Date;
+    isVisible: boolean;
+}
