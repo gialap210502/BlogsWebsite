0adcd403a9b0f0eabaca8d46cadaa4ff4f305ff6
Gialap Duong <89369650+gialap210502@users.noreply.github.com>
2024-03-03 00:05:14 +0700

    Create Auth Controller and Register Action Method(1)

---
+++ API/CodePulse.API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace CodePulse.API.Controllers;
+
+//https://localhost:xxxx/api/blog
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    
+}
