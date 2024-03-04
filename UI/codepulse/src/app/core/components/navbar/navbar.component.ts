486711d177ee53f18f1fa22aedd749850094d02b
Gialap Duong <89369650+gialap210502@users.noreply.github.com>
2024-03-04 10:37:05 +0700

    Implement Logout

--- UI/codepulse/src/app/core/components/navbar/navbar.component.ts
+++ UI/codepulse/src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/features/auth/models/user.model';
 import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
@@ -8,11 +10,10 @@ import { AuthService } from 'src/app/features/auth/services/auth.service';
 })
 export class NavbarComponent implements OnInit{
 
-  /**
-   *
-   */
-  constructor(private authService: AuthService) {
-    
+  user?: User;
+
+  constructor(private authService: AuthService,
+    private router: Router) {
     
   }
 
@@ -20,9 +21,17 @@ export class NavbarComponent implements OnInit{
     this.authService.user()
     .subscribe({
       next: (response) => {
-        console.log(response);
+        this.user = response;
       }
-    })
+    });
+
+    this.user = this.authService.getUser();
+
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
--- UI/codepulse/src/app/features/auth/services/auth.service.ts
+++ UI/codepulse/src/app/features/auth/services/auth.service.ts
@@ -5,6 +5,7 @@ import { LoginResponse } from '../models/login-response.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class AuthService {
 
   $user = new BehaviorSubject<User | undefined>(undefined);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private cookieService: CookieService) { }
 
   login(request: LoginRequest): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${environment.apiBaseUrl}/api/auth/login`, {
@@ -31,5 +33,28 @@ export class AuthService {
 
   user() : Observable<User | undefined> {
     return this.$user.asObservable();
+  }
+
+  getUser(): User | undefined {
+    const email = localStorage.getItem('user-email');
+    const roles = localStorage.getItem('user-roles');
+
+    if(email && roles){
+      const user: User = {
+        email: email,
+        roles: roles?.split(',')
+      }
+
+      return user
+    }
+
+    return undefined;
+
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.cookieService.delete('Authorization', '/');
+    this.$user.next(undefined);
   }
 }
