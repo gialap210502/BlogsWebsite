2ec07cb85f7e6640f702f66bd969d62f19acb1fa
Gialap Duong <89369650+gialap210502@users.noreply.github.com>
2024-03-10 13:17:34 +0700

    UI - Pass JWT Token With API Calls

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -5,31 +5,41 @@ import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment';
 import { UpdateCategoryRequest } from '../models/update-category-request.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private cookieService: CookieService) { }
 
-  addCategory(model: AddCategoryRequest): Observable<void> {
-      return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories`, model);
-  }
 
-  getAllCategories(): Observable<Category[]>{
+
+  getAllCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
   }
 
-  getcategoryById(id: string): Observable<Category>{
+  getcategoryById(id: string): Observable<Category> {
     return this.http.get<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`);
   }
 
-  updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest) : Observable<Category> {
-    return this.http.put<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`, updateCategoryRequest);
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories`, model);
   }
 
-  deleteCategory(id: string) : Observable<Category> {
+  updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest): Observable<Category> {
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`, 
+    updateCategoryRequest,
+    {
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
+  }
+
+  deleteCategory(id: string): Observable<Category> {
     return this.http.delete<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`);
   }
 }
